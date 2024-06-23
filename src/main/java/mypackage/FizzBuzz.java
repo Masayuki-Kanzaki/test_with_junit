54173a654cd030b56d0578b2bef1373815155ec9
Masayuki-Kanzaki <mk199967@gmail.com>
2024-06-23 15:09:44 +0900

    pass in "Fizz"

--- src/main/java/mypackage/FizzBuzz.java
+++ src/main/java/mypackage/FizzBuzz.java
@@ -8,6 +8,9 @@ public class FizzBuzz {
     }
 
     public static String fizzBuzz(int i) {
+        if (i % 3 == 0) {
+            return "Fizz";
+        }
         return null;
     }
 }
--- src/test/java/mypackage/FizzBuzzTest.java
+++ src/test/java/mypackage/FizzBuzzTest.java
@@ -12,4 +12,22 @@ class FizzBuzzTest {
         assertEquals("FizzBuzz",FizzBuzz.fizzBuzz(30));
         assertEquals("FizzBuzz",FizzBuzz.fizzBuzz(90));
     }
+    @Test
+    void fizz() {
+        assertEquals("Fizz",FizzBuzz.fizzBuzz(3));
+        assertEquals("Fizz",FizzBuzz.fizzBuzz(6));
+        assertEquals("Fizz",FizzBuzz.fizzBuzz(12));
+    }
+    @Test
+    void buzz() {
+        assertEquals("Buzz",FizzBuzz.fizzBuzz(5));
+        assertEquals("Buzz",FizzBuzz.fizzBuzz(10));
+        assertEquals("Buzz",FizzBuzz.fizzBuzz(20));
+    }
+    @Test
+    void number() {
+        assertEquals("1",FizzBuzz.fizzBuzz(1));
+        assertEquals("4",FizzBuzz.fizzBuzz(4));
+        assertEquals("17",FizzBuzz.fizzBuzz(17));
+    }
 }
